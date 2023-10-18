5a1fdd5eb94cdaa2b1fc4b578db330d1ccd70520
Taduko <leopoldoj.calix@gmail.com>
2023-10-17 20:59:05 -0600

    Definicion de límites de variables

--- Tareas/Variables_Pyhton.py
+++ Tareas/Variables_Pyhton.py
@@ -3,6 +3,13 @@ decimal = 1.1
 cadena = "Hola"
 booleano = True
 
+
 #Concatenando todas las variables
 
 print("Entero: " + str(entero) + " Decimal: " + str(decimal) + " Cadena: " + cadena + " Booleano: " + str(booleano))
+
+
+#El límite de las variables de tipo entero es de 2^31 - 1
+#El límite de las variables de tipo decimal es de 2^63 - 1
+#El límite de las variables de tipo cadena es de 2^63 - 1
+#El límite de las variables de tipo booleano es de 2^63 - 1
