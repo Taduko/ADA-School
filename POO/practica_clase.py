44571fc69d9d10eb98c91128bc8d0b99b300be85
Leopoldo José Cálix Parada <leopoldoj.calix@gmail.com>
2023-10-19 21:34:16 -0600

    Clases

---
+++ POO/practica_clase.py
@@ -0,0 +1,21 @@
+class Coche:
+    def __init__(self,marca,modelo):
+        self.marca = marca
+        self.modelo = modelo
+        self.velocidad = 0
+
+    def acelerar(self,cantidad):
+        self.velocidad += cantidad
+
+    def frenar(self,cantidad):
+        self.velocidad -= cantidad
+    
+
+mi_coche = Coche("Ford", "Focus")
+
+
+mi_coche.acelerar(50)
+print(mi_coche.velocidad)
+
+mi_coche.frenar(20)
+print(mi_coche.velocidad)
