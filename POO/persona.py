236987ca68dac82b885aed0c61e4b3fe5f7780b2
Leopoldo José Cálix Parada <leopoldoj.calix@gmail.com>
2023-10-19 21:02:30 -0600

    Ejercicio de POO Ejemplo

---
+++ POO/persona.py
@@ -0,0 +1,11 @@
+class Persona:
+    def __init__(self,nombre,edad,sexo):
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
+
+
+persona1 = Persona("Juan", 28, "M")
+
+print(persona1.nombre)
+        
