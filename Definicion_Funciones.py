f137f6870b4866b9fec091aac5fbd3785efa8da1
Leopoldo José Cálix Parada <leopoldoj.calix@gmail.com>
2023-10-26 23:15:38 -0600

    Practicando funciones

---
+++ Definicion_Funciones.py
@@ -0,0 +1,6 @@
+def saludar_nombre(nombre,mensaje):
+    print(f'Hola {nombre}, un gusto conocerte. Que te gustaria comer el dia de hoy? {mensaje}')
+    
+
+
+mensaje_total = saludar_nombre('Juan','Hamburguesa')
